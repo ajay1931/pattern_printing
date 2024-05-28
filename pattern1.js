f90e9ed53182f1338b5ec72b3566779a139cc990
ajay1931 <ajayprakash032@gmail.com>
2024-05-28 06:36:43 +0530

    pattern printing problems init

---
+++ pattern1.js
@@ -0,0 +1,17 @@
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= 5; j++) {
+        process.stdout.write("+i");
+
+    }
+    console.log("");
+}
+
+
+
+// for (let i = 1; i <= 5; i++) {
+//     let row = "";
+//     for (let j = 1; j <= 5; j++) {
+//        row += i + " ";
+//     }
+//     console.log(row);
+// }
